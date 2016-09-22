6bf4886f1def5cf768dccb5553699651aa8eeac4
Tobi Oduah <tobi.oduah@andela.com>
2016-09-22 15:56:58 +0100

    Limit following to successful follow-backs

--- app/workers/follow_worker.rb
+++ app/workers/follow_worker.rb
@@ -8,13 +8,17 @@ class FollowWorker
                 .merge(OauthAccount.where.not(id: oauth_id))
                 .pluck(:username, :oauth_token)
                 .each do |username, token|
+      begin
+        Github::Client::Users::Followers.new(
+          oauth_token: token
+        ).follow current_account.username
+      rescue
+        next
+      end
+
       Github::Client::Users::Followers.new(
         oauth_token: current_account.oauth_token
       ).follow username
-
-      Github::Client::Users::Followers.new(
-        oauth_token: token
-      ).follow current_account.username
     end
   end
 end
